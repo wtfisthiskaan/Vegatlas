e456ad235e693a118648317d01f0486a5e8d6cc8
metevarol <metevarol55@gmail.com>
2022-11-07 14:36:02 +0300

    map pin
    map pinned

--- Vegatlas/Vegatlas/View/MapView.swift
+++ Vegatlas/Vegatlas/View/MapView.swift
@@ -7,10 +7,26 @@
 
 import SwiftUI
 import MapKit
+
+
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+let annotations = [
+        Location(name: "Rulo Lezzetler", coordinate: CLLocationCoordinate2D(latitude: 40.988471, longitude: 29.027615)),
+        Location(name: "VeganArsist", coordinate: CLLocationCoordinate2D(latitude: 40.99160, longitude: 29.072880)),
+]
+
 struct MapView: View {
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 40.989044, longitude: 29.023411), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+    
     var body: some View {
         NavigationView{
-            Map(coordinateRegion: .constant(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 40.989044, longitude: 29.023411), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))))
+            Map(coordinateRegion: $region, annotationItems: annotations) {
+                MapMarker(coordinate: $0.coordinate,tint: Color("Green"))
+            }
                 .toolbarBackground(Color("Green"), for: .navigationBar)
                 .toolbarBackground(.visible, for: .navigationBar)
                 .toolbar {
